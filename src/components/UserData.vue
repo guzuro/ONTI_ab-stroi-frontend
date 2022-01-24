2ddb28a211e66b7a1856338e7b41b3940b12c5c2
guzuro <guzurogmn@gmail.com>
2022-01-24 23:37:08 +0300

    user info component

---
+++ src/components/UserData.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="client-data">
+    <div class="card">
+      <div class="card-header p-2">Информация о пользователе</div>
+      <div class="card-content">
+        <div class="content">
+          <div class="list">
+            <div class="list__item is-flex is-justify-content-space-between">
+              <p class="list__property_left is-size-6 has-text-weight-bold">ID</p>
+              <p class="list__property_right">{{ user.id }}</p>
+            </div>
+            <div class="list__item is-flex is-justify-content-space-between">
+              <p class="list__property_left is-size-6 has-text-weight-bold">Логин</p>
+              <p class="list__property_right">{{ user.login }}</p>
+            </div>
+            <div class="list__item is-flex is-justify-content-space-between">
+              <p class="list__property_left is-size-6 has-text-weight-bold">Имя</p>
+              <p class="list__property_right">{{ user.first_name }}</p>
+            </div>
+            <div class="list__item is-flex is-justify-content-space-between">
+              <p class="list__property_left is-size-6 has-text-weight-bold">Фамилия</p>
+              <p class="list__property_right">{{ user.last_name }}</p>
+            </div>
+            <div class="list__item is-flex is-justify-content-space-between">
+              <p class="list__property_left is-size-6 has-text-weight-bold">Роль</p>
+              <p class="list__property_right">{{ user.role }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- "id" : 53, "login" : "1", "password" : null, "first_name" : "1", "last_name" : "1",
+    "role" : "CUSTOMER", "created_at" : null, "order_id" : 10, "invited_by" : null -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+@Component
+export default class UserData extends Vue {
+  @Prop() user!: any;
+}
+</script>
--- src/views/user-profile.vue
+++ src/views/user-profile.vue
@@ -1,44 +1,19 @@
 <template>
   <div class="user-profile">
-    <div class="card">
-      <div class="card-header p-2">Информация о пользователе</div>
-      <div class="card-content">
-        <div class="content">
-          <div class="list">
-            <div class="list__item is-flex is-justify-content-space-between">
-              <p class="list__property_left is-size-6 has-text-weight-bold">
-                ID
-              </p>
-              <p class="list__property_right">{{ userData.id }}</p>
-            </div>
-            <div class="list__item is-flex is-justify-content-space-between">
-              <p class="list__property_left is-size-6 has-text-weight-bold">Логин</p>
-              <p class="list__property_right">{{ userData.login }}</p>
-            </div>
-            <div class="list__item is-flex is-justify-content-space-between">
-              <p class="list__property_left is-size-6 has-text-weight-bold">Имя</p>
-              <p class="list__property_right">{{ userData.first_name }}</p>
-            </div>
-            <div class="list__item is-flex is-justify-content-space-between">
-              <p class="list__property_left is-size-6 has-text-weight-bold">Фамилия</p>
-              <p class="list__property_right">{{ userData.last_name }}</p>
-            </div>
-            <div class="list__item is-flex is-justify-content-space-between">
-              <p class="list__property_left is-size-6 has-text-weight-bold">Роль</p>
-              <p class="list__property_right">{{ userData.role }}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <user-data :user="userData" />
   </div>
 </template>
 
 <script lang="ts">
 import Component from 'vue-class-component';
 import Vue from 'vue';
+import UserData from '@/components/UserData.vue';
 
-@Component
+@Component({
+  components: {
+    UserData,
+  },
+})
 export default class UserProfile extends Vue {
   get userData(): any {
     return this.$store.getters['userModule/getUserData'];
